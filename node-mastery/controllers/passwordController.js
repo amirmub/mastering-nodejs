38071f7cc89ddfac71c3d9848e9d6ad2072f785c
amirmub <amirmubarek01@gmail.com>
2025-09-29 22:34:27 +0300

    feat: implement email sending functionality

--- node-mastery/controllers/passwordController.js
+++ node-mastery/controllers/passwordController.js
@@ -1,5 +1,6 @@
 const User = require("../models/userModel");
 const crypto = require("crypto");
+const sendEmail = require("../utils/email");
 
 async function forgotPassword(req, res) {
   try {
@@ -12,36 +13,52 @@ async function forgotPassword(req, res) {
     // Generate raw reset token
     const resetToken = crypto.randomBytes(32).toString("hex");
     console.log("Reset token:", resetToken);
-    
+
     // Hash and store in DB
-    const updatedToken = user.passwordResetToken = crypto
+    user.passwordResetToken = crypto
       .createHash("sha256")
       .update(resetToken)
       .digest("hex");
-      
-    console.log("Updated token:", updatedToken);
 
     // Set expiration (10 minutes)
     user.passwordResetExpires = Date.now() + 10 * 60 * 1000;
-
     await user.save({ validateBeforeSave: false });
 
-    // // Send raw token (not hashed) in URL
-    // const resetURL = `${req.protocol}://${req.get(
-    //   "host"
-    // )}/api/v1/reset-password/${resetToken}`;
+    // Send raw token (not hashed) in URL
+    const resetURL = `${req.protocol}://${req.get("host")}/api/v1/reset-password/${resetToken}`;
 
-    // console.log("Reset URL:", resetURL);
+    const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email!`;
 
-    res.status(200).json({
-      status: "success",
-      message: "Password reset link sent to email!",
-    });
+    try {
+      await sendEmail({
+        email: user.email,
+        subject: "Your password reset token (valid for 10 min)",
+        text: message,
+      });
+
+      return res.status(200).json({
+        status: "success",
+        message: "Token sent to email!",
+      });
+    } catch (error) {
+      user.passwordResetToken = undefined;
+      user.passwordResetExpires = undefined;
+      await user.save({ validateBeforeSave: false });
+      console.log(error);
+      
+
+      return res.status(500).json({
+        status: "error",
+        message: "There was an error sending the email.",
+      });
+    }
+
   } catch (error) {
-    console.log(error);
-   return res.status(500).json({ status: "error", message: error.message });
+    console.error(error);
+    return res.status(500).json({ status: "error", message: error.message });
   }
 }
+
 
 async function resetPassword(req, res) {
   // Logic for password reset (send reset email)
