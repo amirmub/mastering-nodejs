25b2a4681606650131657167f44dfeec849d01bd
amirmub <amirmubarek01@gmail.com>
2025-10-03 17:08:55 +0300

    feat: implement rate limiting functionality for security reason

--- node-mastery/app.js
+++ node-mastery/app.js
@@ -2,6 +2,18 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+// implement rate limiting to all requests
+const rateLimit = require("express-rate-limit");
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window`
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  message: "Too many requests from this IP, please try again after 15 minutes"
+});
+app.use(limiter);
+
 app.use(express.json());
 
 // db connection file
